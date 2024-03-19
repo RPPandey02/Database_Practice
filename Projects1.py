5601d13f3f10ec506f1cca1e9902e7ec6dc5a956
Rudra <rudra2462002@gmail.com>
2024-03-19 20:58:22 +0530

    Project for practice

---
+++ Projects1.py
@@ -0,0 +1,18 @@
+"""Task for today:
+
+dataset https://archive.ics.uci.edu/ml/datasets/Bag+of+Words
+
+q1 try to find out a count of each and every word in a respective file return a list of tuple with word and its respective count sample example [('sudh', 6), ('kumar',3)]
+
+A
+
+Spa
+
+q2 = try to perform a reduce operation to get a count of all the word starting with same alphabet sample examle = [(a,56), (b,34),]
+
+q3 Try to filter out all the words from dataset.
+
+001.abstract = abstract
+
+=.002 = delete q4 create a tuple set of all the records avaialble in all the five file and then store it in sqllite DB. (aah,,354,fdsf,wer)
+"""
